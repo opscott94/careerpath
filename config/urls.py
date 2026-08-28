ce0b4f96b7d7ecab7b0749b3e512fc769b723bee
CollinsKobby <collinsnukporti888@gmail.com>
2026-08-28 13:12:33 +0000

    Add /ping/ health check endpoint to prevent Render spin-down

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.http import JsonResponse
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', lambda r: JsonResponse({'status': 'ok'})),
     path('', include('frontend.urls')),
     path('api/', include('api.urls')),
 ]
+
