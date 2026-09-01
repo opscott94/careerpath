e2251e714961410a1b15f5f8ab0f7940cdb54251
CollinsKobby <collinsnukporti888@gmail.com>
2026-09-01 00:58:44 +0000

    Added career outlook to the program details

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('career-search/', views.career_search, name='career_search'),
     path('ai-career-match/', views.ai_career_match, name='ai_career_match'),
     path('program-details/', views.program_details, name='program_details'),
+    path('career-outlook/', views.career_outlook, name='career_outlook'),
     path('subject-recommendation/', views.subject_recommendation, name='subject_recommendation'),
     path('wassce-check/', views.wassce_check, name='wassce_check'),
     path('universities/', views.universities_list, name='universities_list'),
