c160de110bb4381404712ba915ef327698087db1
CollinsKobby <collinsnukporti888@gmail.com>
2026-09-01 20:54:28 +0000

    Updated admin interface

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 from django.http import JsonResponse
 
+admin.site.site_header = "CareerPath Ghana Administration"
+admin.site.site_title = "CareerPath Ghana Admin Portal"
+admin.site.index_title = "Academic & Admissions Data Management"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ping/', lambda r: JsonResponse({'status': 'ok'})),
